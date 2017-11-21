103e3a294baa34501b285b0b850bda26129b0b17
Gabriel Chiconi <gabriel.chiconi@gmail.com>
2017-11-21 18:45:22 -0200

    Clean

--- publi.sh
+++ publi.sh
@@ -1,7 +1,16 @@
 #!/bin/bash
 
+function build {
+    ng build --prod --output-path docs --base-href /escutajs/
+}
+
+function update_remote {
+    git add .
+    git commit -m "$1"
+    git push -f origin master
+}
+
 # Task b (for build) is for gh-pages only
-[ "$2" == "b" ] && ng build --prod --output-path docs --base-href /escutajs/
-git add .
-git commit -m "$1"
-git push -f origin master
+[ "$2" == "b" ] && build
+update_remote $1
+exit
